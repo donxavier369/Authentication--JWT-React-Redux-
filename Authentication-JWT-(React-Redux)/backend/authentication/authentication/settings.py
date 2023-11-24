bc289f39382d4c109840f5a72a899be19c5d094f
Don Xavier <donxavier369@gmail.com>
2023-11-24 13:46:09 +0530

    set up requirements.txt and .env

--- Authentication-JWT-(React-Redux)/backend/authentication/authentication/settings.py
+++ Authentication-JWT-(React-Redux)/backend/authentication/authentication/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import datetime
 import os
+from decouple import config
+
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s7es46r#_hunlfz@ro^w7d4q!foc@7h68%28k@&9dp%r5^2br9'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,9 +88,9 @@ WSGI_APPLICATION = 'authentication.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'authentication',
-        'USER': 'postgres',
-        'PASSWORD': 'mobin',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
 
